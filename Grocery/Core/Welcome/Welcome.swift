85ad88792f508333a918374b23cfd03248ed652f
Vivek Sehrawat <vivek.sehrawat@publicissapeint.com>
2023-09-04 19:23:28 +0530

    navigation added to welcome screen

--- Grocery/Core/Welcome/Welcome.swift
+++ Grocery/Core/Welcome/Welcome.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct Welcome: View {
+    @State private var isShowingLogin = false
     var body: some View {
         ZStack{
             Image("welcom_bg")
@@ -30,10 +31,14 @@ struct Welcome: View {
                 
                 
                 PrimaryButton(buttonTitle: "Get Started", backgroundColor: .green) {
+                    isShowingLogin = true
                 }
                 .frame(height: 50)
                 .padding(EdgeInsets(top: 0, leading: 20, bottom: 45, trailing: 20))
             }
+            NavigationLink(destination: LoginView(), isActive: $isShowingLogin) {
+                                EmptyView()
+                            }
         }
         .ignoresSafeArea()
     }
