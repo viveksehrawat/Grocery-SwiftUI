2321c3ccb1774de3cad7a775897a85ef9deaef35
Vivek Sehrawat <vivek.sehrawat@publicissapeint.com>
2023-09-04 18:00:52 +0530

    added font and assests

---
+++ Grocery/GroceryApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryApp.swift
+//  Grocery
+//
+//  Created by Vivek Sehrawat on 04/09/23.
+//
+
+import SwiftUI
+
+@main
+struct GroceryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
